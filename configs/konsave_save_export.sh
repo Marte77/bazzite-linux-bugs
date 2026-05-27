8255b0a167d7ce0fa1c4eb80d0bdf11bd5b4331c
Marte77 <martinho.tavares@live.com.pt>
2026-05-27 21:24:01 +0200

    update konsave "2026-05-27-21-24-00"

--- configs/konsave_save_export.sh
+++ configs/konsave_save_export.sh
@@ -1,4 +1,19 @@
 #/bin/bash
+
+# 1. Get the absolute path of the script's location
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+# 2. Get the absolute path of the user's current working directory
+CURRENT_DIR="$(pwd)"
+
+# 3. Compare the two directories
+if [ "$SCRIPT_DIR" != "$CURRENT_DIR" ]; then
+    cd $SCRIPT_DIR
+    #echo "Error: This script must be executed from within its own folder. ($SCRIPT_DIR)"
+    #exit 1
+fi
+
+
 DATE="$(date +\"%Y-%m-%d-%H-%M-%S\")"
 git pull
 PROFILE_NAME="kde_plasma_cachy_os"
